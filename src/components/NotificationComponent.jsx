21ad7981f542b815866115f8589c632aa48d357c
jyothipriya2002 <pandugajyothi259@gmail.com>
2025-04-14 13:42:45 +0530

    add

--- src/components/NotificationComponent.jsx
+++ src/components/NotificationComponent.jsx
@@ -1,25 +1,4 @@
-// import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { clearNotifications } from '../features/notificationSlice';
 
-// const NotificationComponent = () => {
-//   const notifications = useSelector(state => state.notification);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div>
-    
-//       <button onClick={() => dispatch(clearNotifications())}>Clear</button>
-//       <ul>
-//         {notifications.map((note, index) => (
-//           <li key={index}>{note.time} - {note.message}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default NotificationComponent;
 
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
